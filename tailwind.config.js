c209005b97fb4a38472c2dd36911ee0816fd71ce
ramavevedharma@gmail.com <ramavevedharma@gmail.com>
2022-06-05 23:19:42 +0700

    portfolio rama

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container: {
+      center: true,
+      padding: '100px',
+    },
+    extend: {
+      colors: {
+        primary: '#05ffa1',
+      },
+      screens: {
+        '2xl': '1320px'
+      }
+    },
+  },
+  plugins: [],
+}
